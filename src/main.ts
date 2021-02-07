6e3eb384d4dfbb60af52dc8181c433fafac93f07
jassmanni <jassmanniq@gmail.com>
2021-02-07 17:13:50 -0400

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
   const port = configService.get(ConfigOptions.port);
+  app.enableCors();
 
   // generate ormconfig.json
   generateTypeormConfigFile(configService);
